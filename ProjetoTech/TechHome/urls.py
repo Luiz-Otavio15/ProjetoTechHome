75f80668a080e63f5841133ccb6526e69ff490e0
Luiz-Otavio15 <oluiz9790@gmail.com>
2026-03-23 01:12:03 -0300

    finalizado umas das funções desse projeto

--- ProjetoTech/TechHome/urls.py
+++ ProjetoTech/TechHome/urls.py
@@ -1,6 +1,7 @@
 from . import views
 from django.urls import path
+app_name = "TechHome"
 
 urlpatterns = [
-    path('', views.tela_principal_tech, name='tela_principal' ),
+    path('', views.tela_principal_tech, name='tela_principal' )
 ]
--- ProjetoTech/TechHome/views.py
+++ ProjetoTech/TechHome/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpRequest
+from django.shortcuts import render, get_object_or_404
 
-from .models import *
+from .models import Produto
 
 # Create your views here.
 
 def tela_principal_tech(request):
     produtos = Produto.objects.all()
-    return render(request, 'html/Tela1Home.html', {'produtos': produtos})
+    barra = request.GET.get('barra')
+
+    if barra:
+        produtos = produtos.filter(nome__icontains=barra)
+
+
+    return render(request, 'html/Tela1Home.html', {
+        'produtos': produtos,
+        'barra': barra
+        },)
